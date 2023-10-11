bd15bac345d1514dff1aeda4163944fb47d50efb
kukkich <vitia.iezhovqqqq@gmail.com>
2023-10-11 21:48:13 +0700

    Добавил DI

---
+++ MysticEchoes/App.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace MysticEchoes;
+
+public class App : Application
+{
+    readonly MainWindow mainWindow;
+
+    public App(MainWindow mainWindow)
+    {
+        this.mainWindow = mainWindow;
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        mainWindow.Show();
+        base.OnStartup(e);
+
+    }
+}
---
+++ MysticEchoes/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MysticEchoes;
+
+public class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        // создаем хост приложения
+        var host = Host.CreateDefaultBuilder()
+            // внедряем сервисы
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<App>();
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+        // получаем сервис - объект класса App
+        var app = host.Services.GetService<App>();
+        // запускаем приложения
+        app?.Run();
+    }
+}
