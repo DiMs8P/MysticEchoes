6091c2971455632e9cc0bd57741e628a1a7424cd
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-01-06 13:08:08 +0700

    added EcsTrees

---
+++ MysticEchoes.Core/AI/NecromancerBt.cs
@@ -0,0 +1,17 @@
+﻿using Leopotam.EcsLite;
+using MysticEchoes.Core.AI.BehaviorTree;
+
+namespace MysticEchoes.Core.AI;
+
+public class NecromancerBt : EcsBt
+{
+    public NecromancerBt(EcsWorld world, int ownerEntityId) : base(world, ownerEntityId){}
+    protected override Node SetupTree()
+    {
+        Node root = new Node();
+
+        return root;
+    }
+
+
+}
