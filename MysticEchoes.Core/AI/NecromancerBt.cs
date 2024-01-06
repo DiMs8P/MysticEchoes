895fab26fd16bf85054d790fc8fa26245fdcc81a
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-01-06 13:26:50 +0700

    added attack node

--- MysticEchoes.Core/AI/NecromancerBt.cs
+++ MysticEchoes.Core/AI/NecromancerBt.cs
@@ -1,5 +1,6 @@
 ﻿using Leopotam.EcsLite;
 using MysticEchoes.Core.AI.BehaviorTree;
+using MysticEchoes.Core.AI.Tasks;
 
 namespace MysticEchoes.Core.AI;
 
@@ -8,10 +9,8 @@ public class NecromancerBt : EcsBt
     public NecromancerBt(EcsWorld world, int ownerEntityId) : base(world, ownerEntityId){}
     protected override Node SetupTree()
     {
-        Node root = new Node();
+        Node root = new Attack(_world, _ownerEntityId);
 
         return root;
     }
-
-
 }
---
+++ MysticEchoes.Core/AI/Tasks/Attack.cs
@@ -0,0 +1,41 @@
+﻿using Leopotam.EcsLite;
+using MysticEchoes.Core.AI.BehaviorTree;
+using MysticEchoes.Core.Movement;
+using MysticEchoes.Core.Player;
+using MysticEchoes.Core.Shooting;
+
+namespace MysticEchoes.Core.AI.Tasks;
+
+public class Attack : Node
+{
+    private readonly EcsWorld _world;
+
+    private readonly EcsPool<TransformComponent> _transforms;
+    private readonly EcsPool<RangeWeaponComponent> _weapons;
+    private readonly int _playerEntityId;
+    private readonly int _ownerEntityId;
+    public Attack(EcsWorld world, int ownerEntityId)
+    {
+        _world = world;
+        _ownerEntityId = ownerEntityId;
+        _transforms = _world.GetPool<TransformComponent>();
+
+        EcsFilter playerFilter = world.Filter<PlayerMarker>().End();
+
+        foreach (var playerEntityId in playerFilter)
+        {
+            _playerEntityId = playerEntityId;
+        }
+    }
+
+    public override NodeState Evaluate()
+    {
+        ref TransformComponent playerTransform = ref _transforms.Get(_playerEntityId);
+        ref TransformComponent ownerTransform = ref _transforms.Get(_ownerEntityId);
+
+        ref RangeWeaponComponent rangeWeaponComponent = ref _weapons.Get(_ownerEntityId);
+        rangeWeaponComponent.IsShooting = true;
+
+        return NodeState.Running;
+    }
+}
