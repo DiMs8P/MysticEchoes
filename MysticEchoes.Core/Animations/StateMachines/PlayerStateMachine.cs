ad499881ff6aed62f8216460d38f8c296f35ed5d
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-01-06 21:44:02 +0700

    small statee machine change

--- MysticEchoes.Core/Animations/StateMachines/PlayerStateMachine.cs
+++ MysticEchoes.Core/Animations/StateMachines/PlayerStateMachine.cs
@@ -4,17 +4,29 @@ namespace MysticEchoes.Core.Animations.StateMachines;
 
 public class PlayerStateMachine : BaseStateMachine
 {
+    private EcsPool<AnimationComponent> _animations;
+    private EcsPool<CharacterAnimationComponent> _characterAnimations;
     public PlayerStateMachine(int ownerEntityId, EcsWorld world) : base(ownerEntityId, world)
     {
+        _animations = world.GetPool<AnimationComponent>();
+        _characterAnimations = world.GetPool<CharacterAnimationComponent>();
     }
 
     public override void Update()
     {
-        return;
+        ref AnimationComponent animationComponent = ref _animations.Get(OwnerEntityId);
+        ref CharacterAnimationComponent characterAnimationComponent = ref _characterAnimations.Get(OwnerEntityId);
+
+        CharacterState currentState = GetCurrentState();
+        
+        if (currentState is CharacterState.Idle)
+        {
+            animationComponent.AnimationId = characterAnimationComponent.Animations[CharacterState.Idle];
+        }
     }
 
     public override CharacterState GetCurrentState()
     {
-        throw new NotImplementedException();
+        return CharacterState.Idle;
     }
 }
