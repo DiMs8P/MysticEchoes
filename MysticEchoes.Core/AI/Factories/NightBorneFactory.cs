e1792c7574217320f2b4bed57728ae60c49c4f35
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-01-08 14:56:53 +0700

    added explosion

--- MysticEchoes.Core/AI/Factories/NightBorneFactory.cs
+++ MysticEchoes.Core/AI/Factories/NightBorneFactory.cs
@@ -4,6 +4,7 @@ using MysticEchoes.Core.Animations.StateMachines;
 using MysticEchoes.Core.Base.Geometry;
 using MysticEchoes.Core.Collisions;
 using MysticEchoes.Core.Collisions.Tree;
+using MysticEchoes.Core.Damage;
 using MysticEchoes.Core.Items;
 using MysticEchoes.Core.Loaders;
 using MysticEchoes.Core.Loaders.Prefabs;
@@ -14,8 +15,10 @@ namespace MysticEchoes.Core.AI.Factories;
 
 public class NightBorneFactory : BaseEnemyFactory
 {
+    private EcsPool<DamageZoneComponent> _damageZones;
     public NightBorneFactory(EcsWorld world, EntityBuilder builder, ItemsFactory itemsFactory, PrefabManager prefabManager) : base(world, builder, itemsFactory, prefabManager)
     {
+        _damageZones = world.GetPool<DamageZoneComponent>();
     }
     
     public override int Create(EnemyInitializationInfo enemyInitializationInfo)
@@ -38,5 +41,34 @@ public class NightBorneFactory : BaseEnemyFactory
         ));
         
         return createdEntity;
+    }
+
+    protected override int CreateDeathAnimationEntity(int entityId, string deathAnimationId)
+    {
+        int animationEntityId = base.CreateDeathAnimationEntity(entityId, deathAnimationId);
+        
+        ref DynamicCollider dynamicCollider = ref _colliders.Has(animationEntityId)
+            ? ref _colliders.Get(animationEntityId)
+            : ref _colliders.Add(animationEntityId);
+
+        ref TransformComponent animationTransform = ref _transforms.Get(animationEntityId);
+        
+        Vector2 boxSize = new Vector2(0.265f, 0.35f) * animationTransform.Scale;
+        dynamicCollider.Box = new Box(animationEntityId, new Rectangle(
+            animationTransform.Location - boxSize / 2, 
+            boxSize
+        ));
+        dynamicCollider.Behavior = CollisionBehavior.Ignore;
+
+        DamageZoneComponent damageZoneComponent = new DamageZoneComponent();
+        Builder.AddTo(animationEntityId, damageZoneComponent);
+
+        DamageComponent damageComponent = new DamageComponent
+        {
+            Damage = 10
+        };
+        Builder.AddTo(animationEntityId, damageComponent);
+
+        return animationEntityId;
     }
 }
---
+++ MysticEchoes.Core/Animations/AnimNotifies/OnEnableDamageZoneNotify.cs
@@ -0,0 +1,44 @@
+﻿using Leopotam.EcsLite;
+using MysticEchoes.Core.Collisions;
+using MysticEchoes.Core.Damage;
+
+namespace MysticEchoes.Core.Animations.AnimNotifies;
+
+public class OnEnableDamageZoneNotify : AnimNotify
+{
+    public override void Notify(int animationOwnerEntityId, EcsWorld world)
+    {
+        SetupDynamicCollider(animationOwnerEntityId, world);
+        SetupDamageZone(animationOwnerEntityId, world);
+    }
+
+    private void SetupDynamicCollider(int entityId, EcsWorld world)
+    {
+        EcsPool<DynamicCollider> dynamicColliders = world.GetPool<DynamicCollider>();
+        
+        if (!dynamicColliders.Has(entityId))
+        {
+            throw new Exception($"Entity {entityId} must have DynamicCollider component.");
+        }
+        
+        ref DynamicCollider dynamicCollider = ref dynamicColliders.Get(entityId);
+        dynamicCollider.Behavior = CollisionBehavior.DamageZone;
+    }
+    
+    private void SetupDamageZone(int animationOwnerEntityId, EcsWorld world)
+    {
+        EcsPool<DamageZoneComponent> damageZones = world.GetPool<DamageZoneComponent>();
+
+        if (damageZones.Has(animationOwnerEntityId))
+        {
+            ref DamageZoneComponent damageZoneComponent = ref damageZones.Get(animationOwnerEntityId);
+            damageZoneComponent.EntityToDamage.Clear();
+            damageZoneComponent.DamagedEntities.Clear();
+        }
+        else
+        {
+            DamageZoneComponent damageZone = new DamageZoneComponent();
+            damageZones.Add(animationOwnerEntityId) = damageZone;
+        }
+    }
+}
