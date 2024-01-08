5f374bb1cf374a3a4a1c6ee1bd979af85eb8d3e9
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-01-08 14:11:49 +0700

    added death

---
+++ MysticEchoes.Core/Animations/AnimNotifies/OnRemoveNotify.cs
@@ -0,0 +1,27 @@
+﻿using Leopotam.EcsLite;
+using MysticEchoes.Core.Scene;
+
+namespace MysticEchoes.Core.Animations.AnimNotifies;
+
+public class OnRemoveNotify : AnimNotify
+{
+    public override void Notify(int animationOwnerEntityId, EcsWorld world)
+    {
+        EcsPool<LifeTimeComponent> lifetimes = world.GetPool<LifeTimeComponent>();
+
+        if (lifetimes.Has(animationOwnerEntityId))
+        {
+            ref LifeTimeComponent lifeTimeComponent = ref lifetimes.Get(animationOwnerEntityId);
+            lifeTimeComponent.LifeTime = 0.0f;
+            lifeTimeComponent.IsActive = true;
+        }
+        else
+        {
+            lifetimes.Add(animationOwnerEntityId) = new LifeTimeComponent()
+            {
+                LifeTime = 0.0f,
+                IsActive = true
+            };
+        }
+    }
+}
