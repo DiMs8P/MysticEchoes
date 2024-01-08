8349e945886ac48635471c9272ea52278e55b816
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2024-01-08 16:43:16 +0700

    hotfix

---
+++ MysticEchoes.Core/DebugContent/EnemySpawnerSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using Leopotam.EcsLite;
+using MysticEchoes.Core.AI;
+using MysticEchoes.Core.AI.Factories;
+using SevenBoldPencil.EasyDi;
+
+namespace MysticEchoes.Core.Debug;
+
+public class EnemySpawnerSystem : IEcsInitSystem
+{
+    [EcsInject] private EnemyFactory _enemyFactory;
+    public void Init(IEcsSystems systems)
+    {
+        CreateEnemy();
+    }
+
+    private void CreateEnemy()
+    {
+        EnemyInitializationInfo enemyInitializationInfo = new EnemyInitializationInfo();
+        enemyInitializationInfo.EnemyId = 2;
+        enemyInitializationInfo.Location = new Vector2(1.1714f, 1.2400f);
+
+        _enemyFactory.CreateEnemy(enemyInitializationInfo);
+    }
+}
